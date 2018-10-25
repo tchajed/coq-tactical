55c40ac1e5ef30a4fff0153f4db16475c5859a6e
Tej Chajed <tchajed@mit.edu>
2018-10-25 15:19:22 -0400

    Add add_hypothesis and gen tactics

--- src/Misc.v
+++ src/Misc.v
@@ -16,3 +16,23 @@ Ltac rename_by_type type name :=
   match goal with | x : type |- _ => rename x into name end.
 
 Ltac is_one_goal := let n := numgoals in guard n = 1.
+
+Ltac add_hypothesis pf :=
+  let P := type of pf in
+  lazymatch goal with
+  | [ H: P |- _ ] => fail "already known"
+  | _ => pose proof pf
+  end.
+
+(* borrowed from Software Foundations' LibTactics
+https://softwarefoundations.cis.upenn.edu/plf-current/LibTactics.html *)
+Tactic Notation "gen" constr(X1) :=
+  generalize dependent X1.
+Tactic Notation "gen" constr(X1) constr(X2) :=
+  gen X2; gen X1.
+Tactic Notation "gen" constr(X1) constr(X2) constr(X3) :=
+  gen X3; gen X2; gen X1.
+Tactic Notation "gen" constr(X1) constr(X2) constr(X3) constr(X4) :=
+  gen X4; gen X3; gen X2; gen X1.
+Tactic Notation "gen" constr(X1) constr(X2) constr(X3) constr(X4) constr(X5) :=
+  gen X5; gen X4; gen X3; gen X2; gen X1.
